c5a2ac1047e02dad85991815e4a3be829915674c
kataoka029 <kataoka029@gmail.com>
2020-05-30 00:13:32 +0900

    Enable web socket - server side

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const app = express();
+
 const morgan = require("morgan");
 const cors = require("cors");
+const corsOption = {
+  origin: "http://localhost:3000",
+  credentials: true,
+};
 
-app.use(cors());
-// app.use(express.static("../frontend/public"));
-// app.use(express.static("../frontend/src"));
+app.use(cors(corsOption));
 app.use(morgan("dev"));
+app.use(express.static("public"));
 app.use("", require("./routers/rootRouter.js"));
 app.use("/api", express.json());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);
+// SOCKET.IO TEST
+const server = require("http").createServer(app);
+const io = require("socket.io").listen(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+// SOCKET.IO TEST
+
+const PORT = process.env.PORT || 4000;
+// server.listen(PORT)で繋がる、aoo.listen(PORT)はいらなそ)
+server.listen(PORT);
 console.log(`Server running at ${PORT}`);
