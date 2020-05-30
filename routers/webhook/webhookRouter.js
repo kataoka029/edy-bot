f7114cc33a4b02936b5c590537c4b26b097eb60a
kataoka029 <kataoka029@gmail.com>
2020-05-30 15:38:44 +0900

    Simplify README.md

--- routers/webhook/webhookRouter.js
+++ routers/webhook/webhookRouter.js
@@ -5,12 +5,12 @@ const webhookRouter = express.Router();
 webhookRouter.post("/", bot.lineMiddleware, async (req, res) => {
   try {
     // DBへの追加、リプライ（ioは中でrequireする必要あり）
-    const io = require("../../server.js");
     bot.insertUserMessage(req, res);
     await bot.reply(req, res);
     bot.insertReply(req, res);
 
     // メッセージの受信をクライアントサイドに通知
+    const io = require("../../server.js");
     const event = req.body.events[0];
     io.emit("refetch", { event });
   } catch (err) {
