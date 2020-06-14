644fda415c74594535a9a10d06e0da5c7d2efe67
kataoka029 <kataoka029@gmail.com>
2020-06-15 00:17:54 +0900

    Push message on line_user_id

--- models/messages/create.js
+++ models/messages/create.js
@@ -3,8 +3,12 @@ const knex = require("knex")(config);
 
 const createMessage = (req, res) => {
   const event = req.body[0];
+  // ここでusersテーブルからuserIdをゲットする？
+  const userId =
+    event.source.userId === "Uf42bb47c877c9e5543ca4eda7661e142" ? 10001 : 0;
+  console.log(userId, event.source.userId);
   return knex("messages").insert({
-    user_id: 10001,
+    user_id: userId,
     line_type: event.type,
     line_reply_token: event.replyToken,
     line_user_id: event.source.userId,
