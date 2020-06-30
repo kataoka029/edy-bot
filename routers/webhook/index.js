2c3821e0f80e90ef131bfe6cd094e711d2efa36b
kataoka029 <kataoka029@gmail.com>
2020-06-30 22:04:53 +0900

    To check webhook events again

--- routers/webhook/index.js
+++ routers/webhook/index.js
@@ -7,18 +7,24 @@ const { reply } = require("../../bot");
 const webhookRouter = express.Router();
 webhookRouter.post("/", lineMiddleware, async (req, res) => {
   const events = req.body.events;
-
-  console.log("START WEBHOOK!", events);
-
-  // DBへの追加、リプライ
-  await insertUserMessage(events);
-  reply(events);
-  await insertReply(events);
-
-  // メッセージの受信をクライアントサイドに通知（ioは中でrequireする必要あり）
-  const io = require("../../server.js");
   const event = events[0];
-  io.emit("refetch", { event });
+
+  if (event.type === "message") {
+    console.log("MESSAGE!", event);
+    // DBへの追加、リプライ
+    await insertUserMessage(events);
+    reply(events);
+    await insertReply(events);
+
+    // メッセージの受信をクライアントサイドに通知（ioは中でrequireする必要あり）
+    const io = require("../../server.js");
+    const event = events[0];
+    io.emit("refetch", { event });
+  } else if (event.type === "follow") {
+    console.log("FOLLOW!", event);
+  } else {
+    console.log(`${event.type.toUpperCase()} is not defined yet.`);
+  }
 });
 
 module.exports = webhookRouter;
