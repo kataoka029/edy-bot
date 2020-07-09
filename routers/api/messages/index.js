e6a68b543b8098e6011da3f21ec1811e32f9fa9b
kataoka029 <kataoka029@gmail.com>
2020-07-10 00:14:19 +0900

    Integrate GET /api/messages/:messageId and /latest

--- routers/api/messages/index.js
+++ routers/api/messages/index.js
@@ -8,41 +8,52 @@ messagesRouter.get("/", (req, res) => {
   return [];
 });
 
-messagesRouter.get("/latest", (req, res) => {
-  return knex
-    .raw(
-      `SELECT
-      messages.line_user_id AS "lineUserId",
-      users.id AS "userId",
-      messages.line_message_type AS "messageType",
-      messages.content AS "content",
-      messages.created_at AS "userDate",
-      sub2.count AS "unreadCount"
-    FROM messages
-      INNER JOIN (
-        SELECT
-          line_user_id,
-          MAX(created_at) AS created_at
-        FROM messages
-        GROUP BY line_user_id
-        ) AS sub1
-          ON messages.line_user_id = sub1.line_user_id AND messages.created_at = sub1.created_at
-      LEFT JOIN(
-        SELECT
-          line_user_id,
-          count(*)
-        FROM messages
-        WHERE unread = 1
-        GROUP BY line_user_id
-        ) AS sub2
-          ON messages.line_user_id = sub2.line_user_id
-      LEFT JOIN users
-        ON messages.line_user_id = users.line_user_id
-    ORDER BY messages.created_at DESC`
-    )
-    .then((users) => res.send(users.rows))
-    .catch((err) => res.status(400).send(err.message));
+messagesRouter.get("/:messageId", (req, res) => {
+  const messageId = req.params.messageId;
+  if (messageId === "latest") {
+    return knex
+      .raw(
+        `SELECT
+        messages.line_user_id AS "lineUserId",
+        users.id AS "userId",
+        messages.line_message_type AS "messageType",
+        messages.content AS "content",
+        messages.created_at AS "userDate",
+        sub2.count AS "unreadCount"
+      FROM messages
+        INNER JOIN (
+          SELECT
+            line_user_id,
+            MAX(created_at) AS created_at
+          FROM messages
+          GROUP BY line_user_id
+          ) AS sub1
+            ON messages.line_user_id = sub1.line_user_id AND messages.created_at = sub1.created_at
+        LEFT JOIN(
+          SELECT
+            line_user_id,
+            count(*)
+          FROM messages
+          WHERE unread = 1
+          GROUP BY line_user_id
+          ) AS sub2
+            ON messages.line_user_id = sub2.line_user_id
+        LEFT JOIN users
+          ON messages.line_user_id = users.line_user_id
+      ORDER BY messages.created_at DESC`
+      )
+      .then((users) => res.send(users.rows))
+      .catch((err) => res.status(400).send(err.message));
+  }
+  return knex("messages")
+    .where({ line_message_id: messageId })
+    .select()
+    .then((message) => res.send(message))
+    .then(() => console.log("SUCCESS - GET /messages/:messageId"))
+    .catch((err) => console.log("ERROR - GET /messages/:messageId - ", err));
 });
+
+messagesRouter.get("/latest", (req, res) => {});
 
 messagesRouter.post("/", (req, res) => {
   const events = req.body;
